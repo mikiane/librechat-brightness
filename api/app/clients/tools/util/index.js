f65fff147856af340a2d1ebac9ad309c7490f499
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2025-04-23 17:47:39 +0200

    Update index.js

--- api/app/clients/tools/util/index.js
+++ api/app/clients/tools/util/index.js
@@ -1,8 +1,26 @@
 const { validateTools, loadTools } = require('./handleTools');
 const handleOpenAIErrors = require('./handleOpenAIErrors');
 
+const {
+  availableTools,
+  manifestToolMap,
+  BrowserScraper,
+  // Autres outils que vous souhaitez exporter
+  // Par exemple :
+  // GoogleSearchAPI,
+  // DALLE3,
+  // ...
+} = require('./availableTools'); // Assurez-vous que le chemin est correct
+
 module.exports = {
   handleOpenAIErrors,
   validateTools,
   loadTools,
+  availableTools,
+  manifestToolMap,
+  BrowserScraper,
+  // Exportez également les autres outils si nécessaire
+  // GoogleSearchAPI,
+  // DALLE3,
+  // ...
 };
